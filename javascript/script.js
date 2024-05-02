342f9e8b546d771d0f90a168ca9ec27205efb884
naveen Rajan <sce22cs026@sairamtap.edu.in>
2024-05-03 00:07:49 +0530

    script update

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,20 @@
-const Button = document.querySelector(".placeorder");
+const Button = document.querySelector('.placeorder');
 
 Button.addEventListener('click', () => {
-    cons
-})
+  const name = document.querySelector('.name').value;
+  const address = document.querySelector('.address').value;
+  const CanCount = document.querySelector('.cans').value;
+
+    //calling the function
+  WhatsApp(name, address, CanCount);
+});
+
+function WhatsApp(Name, Address, Count) {
+  const number = '6382817065';
+  const message = encodeURIComponent(
+    `hello im ${Name} and i wish to order ${Count} cans, and this is my Address: ${Address}`
+  );
+
+  const whatsapp = `https://wa.me/${number}?text=${message}`;
+  window.open(whatsapp, '_blank');
+}
